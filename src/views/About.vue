2be4fd03338a89ca43da6faf04686648bd8daa26
Radjartha <radjartha12@gmail.com>
2023-10-29 19:20:43 +0700

    Fix About

--- src/views/About.vue
+++ src/views/About.vue
@@ -7,7 +7,7 @@ import carddev from '../components/carddev.vue';
 
 <template>
 
-  <div class="w-full bg-cover bg-center" style="height:32rem; background-image: url('../../public/assets/img/Bg.png');">
+  <div class="w-full bg-cover bg-center" style="height:32rem; background-image: url('../../public/assets/img/Bg1.webp');">
         <div class="flex items-center justify-center h-full w-full bg-gray-900 bg-opacity-50">
             <div class="text-center">
                 <h1 class="text-white text-2xl font-semibold uppercase md:text-3xl">ZealoFurr</h1>
